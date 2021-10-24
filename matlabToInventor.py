846d28970580a95c2261b239b745d8ab3bddb4f7
Kutlay Hanli <khanli@ku.edu.tr>
2021-10-24 14:04:30 +0300

    mlx to xlsx added

---
+++ matlabToInventor.py
@@ -0,0 +1,52 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='matlabToInventor.py')
+    # Adding optional argument
+    parser.add_argument("-m", "--mFile", type=str, help = "Input Matlab .m file")
+    parser.add_argument("-x", "--csvFile", type=str, help = "Inventor compatible .csv file")
+    args = parser.parse_args()
+    print(args)
+
+    oLines = []
+    
+    # Using readlines()
+    if args.mFile:
+        input = open(args.mFile, 'r')
+        Lines = input.readlines()
+        
+        skip = False
+        count = 0
+        # Strips the newline character
+        for line in Lines:
+            count += 1
+            if "%{" in line and not skip:
+                skip = True
+            elif "%}" in line and skip:
+                skip = False
+            
+            if skip:
+                continue
+
+            if "m2" in line: #Skip areas
+                continue
+            
+            if "=" in line:
+                if "e-3" in line:
+                    line = line.replace("e-3","*0.001")
+                tokens = line.split("=")
+                name = tokens[0].strip()
+                tokens = tokens[1].split("%")
+                value = tokens[0].strip()
+                unit = "ul"
+                if len(tokens) > 1:
+                    unit = tokens[1].strip()
+                print("Param:{}\tValue:{}\tUnit:{}".format(name, value, unit))
+                oLines.append("{},{},{}\n".format(name, value, unit))
+        input.close()
+                
+    # writing to file
+    if args.csvFile:
+        output = open(args.csvFile, 'w')
+        output.writelines(oLines)
+        output.close()
