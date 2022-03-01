8dd9e26a076e47b8b0659d21dbb5f890a4cead18
Kutlay Hanli <khanli@ku.edu.tr>
2022-03-01 15:26:13 +0300

    Improved tank intake/discharge computation

--- tank_discharge_T_io.m
+++ tank_discharge_T_io.m
@@ -1,14 +1,14 @@
-function T1 = tank_discharge_T_io(T0,m0,Tmd,md,gam)
+function [T1, m1] = tank_discharge_T_io(T0,m0,Tmd,md,gam)
 %
 % Calculate temperature after discharge
 %
-%   tank_discharge_T(T0,m0,md,gam)
+%   tank_discharge_T_io(T0,m0,md,gam)
 %
 %   T0: initial temperature in K
 %   m0: initial gas mass in kg
 %   Tmd: temperature of io gas in K
 %   md: gas io in kg
 %   gam: specific heat ratio Cp/Cv
-    T1 = (md * gam * Tmd + m0 * T0)/...
-        (m0 + md);
+    m1 = m0 + sum(md);
+    T1 = (gam * md * Tmd' + m0 * T0)/m1;
 end
